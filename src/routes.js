962541fc334b3a8b6514e3669d3c279c16d61ffb
Jose Castor <jose.neto@totvs.com.br>
2019-06-25 09:13:11 -0300

    Implements userController and create request router to method store

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import userController from './app/controllers/UserController';
+
 const routers = new Router();
 
 routers.get('/', (req, res) => res.json({ status: 'Api On' }));
+
+routers.post('/users', userController.store);
 
 export default routers;
