7b0dce2d30022ce75524cc1efa323cc40c3e7a16
Jose Castor <jose.neto@totvs.com.br>
2019-06-25 11:05:57 -0300

    Add new routers and middleware od authentication

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
+import sessionController from './app/controllers/SessionController';
 import userController from './app/controllers/UserController';
 
 const routers = new Router();
 
 routers.get('/', (req, res) => res.json({ status: 'Api On' }));
 
+routers.post('/sessions', sessionController.store);
 routers.post('/users', userController.store);
+
+routers.use(authMiddleware);
+
+routers.put('/users', userController.update);
 
 export default routers;
