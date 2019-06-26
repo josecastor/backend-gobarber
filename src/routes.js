83ff2438b7f28e432571ebf2ce74c4bfe5f7c158
Jose Castor <jose.neto@totvs.com.br>
2019-06-26 17:03:43 -0300

    Add router schedule

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routers = new Router();
 const upload = multer(multerConfig);
@@ -26,6 +27,8 @@ routers.get('/providers', ProviderController.index);
 
 routers.get('/appointments', AppointmentController.index);
 routers.post('/appointments', AppointmentController.store);
+
+routers.get('/schedule', ScheduleController.index);
 
 routers.post('/files', upload.single('file'), FileController.store);
 
