3115c26900ff43269c08b7616503ce1fdc688565
Jose Castor <jose.neto@totvs.com.br>
2019-06-26 21:04:05 -0300

    Add router notifications

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,15 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import authMiddleware from './app/middlewares/auth';
-
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routers = new Router();
 const upload = multer(multerConfig);
@@ -29,6 +30,8 @@ routers.get('/appointments', AppointmentController.index);
 routers.post('/appointments', AppointmentController.store);
 
 routers.get('/schedule', ScheduleController.index);
+
+routers.get('/notifications', NotificationController.index);
 
 routers.post('/files', upload.single('file'), FileController.store);
 
