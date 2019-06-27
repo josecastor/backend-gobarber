bc0c1557ae69e48403fc8ddd8929407d0445bd13
Jose Castor <jose.neto@totvs.com.br>
2019-06-27 16:43:14 -0300

    Add fields virtual past and cancelable

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,11 +1,27 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
-        date: Sequelize.DATE,
+        date: {
+          type: Sequelize.DATE,
+          timezone: '-0300',
+        },
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
